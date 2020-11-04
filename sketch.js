7bb112d2f09da02a6a95b83a84cce91fef785170
AnnaSiddiqui <70028872+AnnaSiddiqui@users.noreply.github.com>
2020-11-03 20:40:03 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
-var dog, happyDog, database
-var foodS, foodStock
+var dog, happyDog, database;
+var foodS, foodStock;
+var feed, addFood;
+var fedTime, lastFed;
+var foodObj;
 
 function preload()
 {
@@ -17,16 +20,40 @@ function setup() {
   foodStock=database.ref('Food');
   foodStock.on("value",readStock);
 
+  feed = createButton("Feed the dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
+
+  addFood = createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+  
+
+
+
+
 }
 
 
 function draw() {  
  background(46,139,87);
 
-if(keyWentDown(UP_ARROW)){
-  writeStock(foodS);
- // dog.addImage(happyDog);
+fedTime = database.ref('FeedTime');
+fedTime.on("value",function(data){
+  lastFed=data.val();
+});
 
+
+
+fill(255,255,254);
+textSize(15);
+if(lastFed>=12){
+  text("Last Feed : "+ lastFed%12 + " PM", 350,30);
+}else if(lastFed==0){
+  text("Last Feed : 12 AM",350,30);
+}else{
+  text("Last Feed : "+ lastFed + "AM", 350,30);
 }
 
 
@@ -46,4 +73,23 @@ function writeStock(x){
     Food:x
   })
 }
+
+function feedDog(){
+  dog.addImage(happyDog);
+
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour()
+  })
+}
+
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foods
+  })
+}
+
+
 
